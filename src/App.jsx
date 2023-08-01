c1c9025a5775dfa8259b95d6e7e89e7da06587d0
Yongjian Xuan <xcircle08@gmail.com>
2023-08-01 16:37:02 -0700

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
-
-import './App.css'
+import {puppyList} from './data.js';
+import { useState } from 'react';
+import './index.css';
+import './app.css';
 
 function App() {
-
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId);
+  console.log(featuredPup);
+  console.log(puppies);
+  function handleClick() {
+   //console.log("puppy id: ", id)
+   //setFeatPupId(key)
+    /**console.log("key: " + key)*/
+   // some logic here
+  }
   return (
-    <>
-      <div>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={()=>setFeatPupId(puppy.id)} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App
